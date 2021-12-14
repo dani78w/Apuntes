478eb2fc7d340f2ddfca188ef63b129ed73a25e2
dani78w <danielarribas78w@gmail.com>
2021-12-14 09:18:21 +0100

    Push

--- autocommit.bat
+++ autocommit.bat
@@ -1,3 +1,3 @@
-git add -u                               REM :: Only add tracked files
-git commit -m "your commit message"      REM :: Commit all your added files
-git push origin main                  REM :: Branch (i.e. main)
+git add .                         
+git commit -m "Push"     
+git push origin main              
