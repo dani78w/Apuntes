2bbc188298aa36ec604c32329a6330bb2f418df1
dani78w <danielarribas78w@gmail.com>
2021-12-16 12:03:12 +0100

    Push

---
+++ Programación/C/Ejercicio 14 Funciones/Ejercicio.c
@@ -0,0 +1,23 @@
+    //librerias y constantes
+    	#include <stdio.h>
+    	#include <locale.h>
+    	#include <string.h>
+    	#define tam 10
+    	#define ctam 50
+    	
+    	
+    main(){
+    	
+    	setlocale(LC_ALL, "spanish");
+    	char matriz[tam][ctam];
+    	int i = 0;int aux= 0;int a=0; int b=0;
+    	printf("%d",sumaDosNum(a,b));
+    	//iniciacion de matriz
+    	
+		
+	}
+	int sumaDosNum(int num1,int num2){
+    		int suma=0;
+    		suma=num1+num2;
+    		return suma;
+		}
