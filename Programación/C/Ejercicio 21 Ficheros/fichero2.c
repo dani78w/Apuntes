2848652b23290b30b5cd736af254de2f67f9b4e6
dani78w <danielarribas78w@gmail.com>
2022-02-10 11:57:16 +0100

    Push

---
+++ Programación/C/Ejercicio 21 Ficheros/fichero2.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+#define desplaz(n) sizeof(estructura)*(n-1)
+
+struct cd{
+		char nombre[25];
+		char titulo[30];
+	};
+struct cd cd1;
+	
+
+//leer fichero de estructuras
+void main(){
+	
+	FILE*fich;
+	fich=fopen("txt2.txt","w");
+	
+
+	int cont=0;
+	for(cont=1;cont<3;cont++){
+		printf("nombre: ");
+		scanf("%s",&cd1.nombre);
+		fflush(stdin);
+		
+		printf("Titulo: ");
+		scanf("%s",&cd1.titulo);
+		fflush(stdin);
+		
+
+		
+		
+		fwrite(&cd1,sizeof(cd1),1,fich);	
+	};
+	printf("%d",sizeof(cd1));
+	
+	
+	fclose(fich);
+	
+	//segunda parte
+
+	
+};
+
