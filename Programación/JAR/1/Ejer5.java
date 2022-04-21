d17f5df84a77ca7eb116948dde81984d5419100e
dani78w <danielarribas78w@gmail.com>
2022-04-21 12:52:03 +0200

    clase

---
+++ Programación/JAR/1/Ejer5.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class Ejer5 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Ejer5
+{
+    // instance variables - replace the example below with your own
+    public static void main(){
+        /*
+         * 
+        do{
+           
+            * SOP OPCIO1
+            * SOP OPCIO2
+            * SOP 4 SALIR
+            
+           
+        }while opcion distinta 4
+        switch
+        */
+        
+        
+        
+    }
+}
