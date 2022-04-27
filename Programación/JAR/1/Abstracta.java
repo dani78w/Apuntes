82a37df50df2f783228287e0a214c33b26f8366c
dani78w <danielarribas78w@gmail.com>
2022-04-27 14:19:32 +0200

    Push

---
+++ Programación/JAR/1/Abstracta.java
@@ -0,0 +1,14 @@
+/**
+ * Abstract class Abstracta - write a description of the class here
+ * 
+ * @author: 
+ * Date: 
+ */
+public abstract class Abstracta
+{
+    //metodos abstractos necesitan ser redefinidos en la clase derivada
+    public int sampleMethod(int y)
+    {
+    return y;
+    }
+}
