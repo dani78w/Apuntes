1645698d086a5c7c621464c0fd87db726a4e5e43
dani78w <danielarribas78w@gmail.com>
2022-05-04 14:17:48 +0200

    clase

--- Programación/JAR/1/EjercicioExceptions.java
+++ Programación/JAR/1/EjercicioExceptions.java
@@ -1,19 +1,35 @@
+import java.io.UTFDataFormatException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class EjercicioExceptions
 {
-    
-    public static void errorAritmetico(int a, int b){
+    public static void errorAritmetico(int a, int b) throws ArithmeticException{
         int r = b/a ;
     }
-
-
-    public static void main(){
-    int a = 0, b = 300 ;
-    try{
-         errorAritmetico(a,b);
-     }catch(ArithmeticException e){
-         System.out.println("Se ha producido un error" +e);
-     }      
+    public static void bucle(){
+        System.out.println("Introduce 6 numeros enteros seguidos");
+        Scanner sc = new Scanner(System.in);
+        int var = 0;
+        for (int i = 0; i < 6 ; i++) {
+            System.out.println("Introduce un numero entero: ");
+            var = sc.nextInt();
+        }
+        System.out.println("Enorabuena todos los números introducidos son enteros");
+    }
+    public static void main(String[] args){
+        int a = 0, b = 300 ;
+        //ejercicio1
+            try{
+                 errorAritmetico(a,b);
+             }catch(ArithmeticException e){
+                 System.out.println("Se ha producido un error" +e);
+             }
+        //ejercicio3
+            try{
+                bucle();
+            }catch(InputMismatchException e2){
+                System.out.println("el numero introducido no es entero");
+            }
     }
 }
