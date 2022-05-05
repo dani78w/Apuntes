be80da1169b8947ba71b77dad97c94452c11f636
dani78w <danielarribas78w@gmail.com>
2022-05-05 14:16:34 +0200

    Push

--- Programación/JAR/1/ExcepcionesPrincipal.java
+++ Programación/JAR/1/ExcepcionesPrincipal.java
@@ -2,22 +2,30 @@ import java.util.Scanner;
 
 public class ExcepcionesPrincipal {
     public static void pedir(){
-        int numero=0;
+        int numero;
         Scanner sc = new Scanner(System.in);
         System.out.println("Número: ");
         numero=Integer.parseInt(sc.nextLine());
         System.out.println("El número es: "+numero);
     }
     public static void main(String[] args) {
-    //control de errores con try catch finally
-        try {
-                pedir();
-        }catch(NumberFormatException e){
-                System.out.println("Mete un numero entero :C ");
+        /*  control de errores con try catch finally solo
+            las cosas que se ven afectadas cuando ocurre
+            una excepcion */
+            boolean flag=false;
+                do {
+
+                    try {
+                        pedir();
+                        flag=true;
+                    }catch (NumberFormatException e2) {
+                        System.out.println("El caracter introducido no es un entero");
+                    }
+
+                }while (!flag);
+
                 //recuerda usar EXCEPTION para el resto de excepciones no controladas
-        } finally {
-                System.out.println("Programa finalizado");
-        }
+
 
 
 
