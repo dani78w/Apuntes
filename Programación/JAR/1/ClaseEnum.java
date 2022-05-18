7ffca7caf44e9f47ed6135df276953f81baf19d0
dani78w <danielarribas78w@gmail.com>
2022-05-18 13:56:27 +0200

    Push

--- Programación/JAR/1/ClaseEnum.java
+++ Programación/JAR/1/ClaseEnum.java
@@ -1,13 +1,6 @@
-
-/**
- * Write a description of class ClaseEnum here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public enum ClaseEnum
 {   
     /*NO SE PUEDE CREAR UN OBJETO DE UNA CLASE ENUMERADA SE SUELEN UTILIZAR PARA MUCCHOS PROGRAMAS*/
     MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY;
-    
+
 }
--- Programación/JAR/1/PrincipalEnum.java
+++ Programación/JAR/1/PrincipalEnum.java
@@ -1,26 +1,64 @@
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+/*
+            ClaseEnum diaT = ClaseEnum.TUESDAY;
+            ClaseEnum diaJ = ClaseEnum.THURSDAY;
+            System.out.println(diaT.name());
+            System.out.println(ClaseEnum.THURSDAY);
 
-/**
- * Write a description of class PrincipalEnum here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+            System.out.println(diaT.name());
+            System.out.println(ClaseEnum.THURSDAY);
+
+            System.out.println(diaT.ordinal()); //posicion de la constante
+
+            ClaseEnum[] dias = ClaseEnum.values();
+             System.out.println(dias[1].name()); //nombre de la constante
+
+            System.out.println(diaT.compareTo(diaJ));
+            System.out.println(diaT.compareTo(dias[1]));
+            System.out.println(diaT.equals(diaJ)); //comparan por posiciones
+         */
+           
 public class PrincipalEnum
 {
     public static void main(){
-        ClaseEnum diaT = ClaseEnum.TUESDAY;
-        ClaseEnum diaJ = ClaseEnum.THURSDAY;
-        System.out.println(diaT.name());
-        System.out.println(ClaseEnum.THURSDAY);
-        
-        System.out.println(diaT.name());
-        System.out.println(ClaseEnum.THURSDAY);
-        
-        ClaseEnum[] dias = ClaseEnum.values();
-         System.out.println(dias[1].name());
-    
-        System.out.println(diaT.compareTo(diaJ));
-        System.out.println(diaT.compareTo(dias[1]));
-    
+ 
+        System.out.println("-----EJERCICIO-----");
+            //pedir un dia de la semana por pantalla y controlar que ese dia de la semana
+            //sea correcto mediante una clase enumerada
+            ClaseEnum[] dias = ClaseEnum.values();
+            ejercicio1(dias);
+            
     }
+    public static void ejercicio1(ClaseEnum[] dias){
+            Scanner sc = new Scanner(System.in, StandardCharsets.UTF_8);
+            String dia= sc.nextLine();
+            String aux= " ";
+            int tam=0;
+
+            try {
+                do {
+                    aux= String.valueOf(dias[tam]);
+                    tam = tam + 1;
+                }while(tam>0);
+            }catch (java.lang.ArrayIndexOutOfBoundsException e){
+                System.out.println("El tamaño del enum es: "+tam);
+            }
+            int x=0;
+            boolean flag = false;
+            for (int i = 0; i < tam; i++) {
+                if (dia.equalsIgnoreCase(String.valueOf(dias[i]))){
+                    flag=true;
+                    x=i;
+                    
+                }
+            }
+            System.out.println("la posicion del dia dentro del enum es : "+x);
+            if (flag){
+                System.out.println(dia+" existe en el enum");
+            }else {
+                System.out.println(dia+" no existe en el enum");
+            }
+    }
+
 }
